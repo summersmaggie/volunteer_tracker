002c752d9d535c6a3a2fc8026f3e15bcc1fcd2b4
Maggie Summers <summers.maggie@gmail.com>
2018-02-23 10:22:10 -0800

    add sinatra routing to pass first integration spec

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
 require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
-require("./lib/projects")
-require("./lib/volunteers")
+require("./lib/project")
+require("./lib/volunteer")
 require("pg")
 require("pry")
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get('/') do
+  erb(:home)
+end
+
+post('/projects') do
+  title = params.fetch('title')
+  project = Project.new({:title => title, :id => nil})
+  project.save()
+  erb(:project_success)
+end
+
+get('/projects') do
+  @projects = Project.all()
+  erb(:projects)
+end
