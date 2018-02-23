9d6aedebba4acb5f202d91b2f57eef5443b4f682
Maggie Summers <summers.maggie@gmail.com>
2018-02-23 09:29:38 -0800

    add passing code for volunteer method in project class

--- lib/project.rb
+++ lib/project.rb
@@ -26,4 +26,25 @@ class Project
     projects
   end
 
+  def self.find(id)
+    found_project = nil
+    Project.all().each() do |project|
+      if project.id().==(id)
+        found_project = project
+      end
+    end
+    found_project
+  end
+
+  def volunteers
+    project_volunteers = []
+    volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{self.id()};")
+    volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i()
+      id = volunteer.fetch("id").to_i()
+      project_volunteers.push(Volunteer.new(:name => name, :project_id => project_id, :id => id))
+    end
+    project_volunteers
+  end
 end
