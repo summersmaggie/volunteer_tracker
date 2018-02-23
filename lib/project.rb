724d8b66afa3f2b987dbd63118da9f86cd4fcb27
Maggie Summers <summers.maggie@gmail.com>
2018-02-23 09:34:19 -0800

    pass remaining test specs in project class

--- lib/project.rb
+++ lib/project.rb
@@ -47,4 +47,14 @@ class Project
     end
     project_volunteers
   end
+
+  def update(attributes)
+    @title = attributes[:title]
+    @id = self.id()
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+  end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{self.id()};")
+  end
 end
