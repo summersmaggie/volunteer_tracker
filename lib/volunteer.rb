f143a05ea5cbeb8f9cedbf47aa1b1b01bc1ee589
Maggie Summers <summers.maggie@gmail.com>
2018-02-23 08:54:40 -0800

    pass remaining volunteer test specs

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -11,5 +11,31 @@ class Volunteer
     self.name().==(another_volunteer.name()).&(self.id().==(another_volunteer.id()))
   end
 
+  def self.all
+    volunteers = []
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', '#{@project_id}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  def self.find(id)
+    found_volunteer = nil
+    Volunteer.all().each() do |volunteer|
+      if volunteer.id().==(id)
+        found_volunteer = volunteer
+      end
+    end
+    found_volunteer
+  end
 
 end
