32d5f416f486982c9a4f37eba5a9ac4f5d9d6623
Maggie Summers <summers.maggie@gmail.com>
2018-02-23 09:07:19 -0800

    add two more passing test specs for project class

--- lib/project.rb
+++ lib/project.rb
@@ -6,4 +6,24 @@ class Project
     @id = attributes[:id]
   end
 
+  def ==(another_project)
+    self.title().==(another_project.title()).&(self.id().==(another_project.id()))
+  end
+
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first.fetch("id").to_i()
+  end
+
+  def self.all
+    projects = []
+    returned_projects = DB.exec("SELECT * FROM projects")
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i()
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
 end
